d72b3661c67f7039637028e13338f65b5761fe97
Satori <Satoriappdev@gmail.com>
2023-06-14 13:55:32 +0100

    Initial Commit

---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Jareth Cameron on 14/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
